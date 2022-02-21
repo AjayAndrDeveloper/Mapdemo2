9e0a4166a3b0e04e7c71b9a22a629d3f6a94f503
Ajay <ajay.m.beetonz@gmail.com>
2022-02-21 05:39:37 -0800

    update

--- app/src/main/java/com/example/mapdemo2/MapsActivity.java
+++ app/src/main/java/com/example/mapdemo2/MapsActivity.java
@@ -15,109 +15,87 @@ import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.SearchView;
+import android.widget.Spinner;
 
 import com.example.mapdemo2.databinding.ActivityMapsBinding;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMapOptions;
+import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 //import com.example.mapdemo2.databinding.ActivityMapsBinding;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
-   Button normal_map_Btn, hybrid_map_btn, satellite_map_btn, terrain_map_Btn;
+public class MapsActivity extends FragmentActivity {
+   Button zoom_In, zoom_out;
+   SearchView searchView;
    private GoogleMap mMap;
    Marker marker;
    Geocoder geocoder;
-   CameraUpdate centre,zoom;
+   CameraUpdate centre, zoom;
    List<Address> myAddress;
-
-//   @Override
-//   public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-//      super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-//
-//      if (grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-//
-//         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
-//            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,locationListener);
-//
-//            Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-//            centreMapOnLocation(lastKnownLocation,"Your Location");
-//         }
-//      }
-//   }
+   SupportMapFragment mapFragment;
+   private Spinner spinner;
+   private static final String[] paths = {"NORMAL MAP ", "HYBRID MAP", "SATELLITE MAP", "TERRAIN MAP"};
+   FusedLocationProviderClient fusedLocationProviderClient;
 
    @Override
    protected void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
-
-//      binding = ActivityMapsBinding.inflate(getLayoutInflater());
       setContentView(R.layout.activity_maps);
+      zoom_In = findViewById(R.id.zoomIn);
+      zoom_out = findViewById(R.id.zoomOut);
+      spinner = (Spinner) findViewById(R.id.spinner);
+      searchView = findViewById(R.id.search_bar);
+      ArrayAdapter<String> adapter = new ArrayAdapter<String>(MapsActivity.this,
+              android.R.layout.simple_spinner_item, paths);
+      spinner.setAdapter(adapter);
       geocoder = new Geocoder(MapsActivity.this, Locale.getDefault());
-      // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-      SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-              .findFragmentById(R.id.map);
-      mapFragment.getMapAsync(this);
 
-      normal_map_Btn = findViewById(R.id.MapNormalBtn);
-      hybrid_map_btn = findViewById(R.id.MapHybridBtn);
-      satellite_map_btn = findViewById(R.id.MapSatelliteBtn);
-      terrain_map_Btn = findViewById(R.id.MapTerrainBtn);
-      normal_map_Btn.setOnClickListener(new View.OnClickListener() {
-         @Override
-         public void onClick(View view) {
-            mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-         }
-      });
-      hybrid_map_btn.setOnClickListener(new View.OnClickListener() {
-         @Override
-         public void onClick(View view) {
-            mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-         }
-      });
-      satellite_map_btn.setOnClickListener(new View.OnClickListener() {
-         @Override
-         public void onClick(View view) {
-            mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
-         }
-      });
-      terrain_map_Btn.setOnClickListener(new View.OnClickListener() {
-         @Override
-         public void onClick(View view) {
-            mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
-         }
-      });
+      // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+      mapFragment = (SupportMapFragment) getSupportFragmentManager()
+              .findFragmentById(R.id.map);
+
+
+      fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+      if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+         // TODO: Consider calling
+         //    ActivityCompat#requestPermissions
+         // here to request the missing permissions, and then overriding
+         //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+         //                                          int[] grantResults)
+         // to handle the case where the user grants the permission. See the documentation
+         // for ActivityCompat#requestPermissions for more details.
+         getCurrentLocation();
+         return;
+      } else {
+         ActivityCompat.requestPermissions(MapsActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
+
+      }
 
 
    }
 
 
-   /**
-    * Manipulates the map once available.
-    * This callback is triggered when the map is ready to be used.
-    * This is where we can add markers or lines, add listeners or move the camera. In this case,
-    * we just add a marker near Sydney, Australia.
-    * If Google Play services is not installed on the device, the user will be prompted to install
-    * it inside the SupportMapFragment. This method will only be triggered once the user has
-    * installed Google Play services and returned to the app.
-    */
-   @Override
-   public void onMapReady(GoogleMap googleMap) {
-      mMap = googleMap;
-      MarkerOptions markerOptions = new MarkerOptions();
-//       Add a marker in Sydney and move the camera
-//      LatLng sydney = new LatLng(-34, 151);
-//      mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-//      mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+   private void getCurrentLocation() {
 
       if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
          // TODO: Consider calling
@@ -129,75 +107,151 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
          // for ActivityCompat#requestPermissions for more details.
          return;
       }
-      mMap.setMyLocationEnabled(true);
-      mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+//      mMap.setMyLocationEnabled(true);
 
+      Task<Location> task = fusedLocationProviderClient.getLastLocation();
+
+      task.addOnSuccessListener(new OnSuccessListener<Location>() {
          @Override
-         public void onMapClick(LatLng latlng) {
-            // TODO Auto-generated method stub
+         public void onSuccess(Location location) {
+            if (location != null) mapFragment.getMapAsync(new OnMapReadyCallback() {
+               @Override
+               public void onMapReady(GoogleMap googleMap) {
 
-            if (marker != null) {
-               marker.remove();
-            }
 
-            try {
-               myAddress = geocoder.getFromLocation(latlng.latitude, latlng.longitude,1);
-            } catch (IOException ioException) {
-               ioException.printStackTrace();
-            }
-            String address = myAddress.get(0).getPremises();
-            Log.d("address", "onMapClick: " + myAddress.get(0).getAdminArea() + "  "+ myAddress.get(0).getSubAdminArea()+
-                    "     p " + myAddress.get(0).getPremises()+"  myAddress"+ myAddress  +"    SUBlOCALOITY"+ myAddress.get(0).getSubLocality() + "  FACE"+ myAddress.get(0).getThoroughfare());
-            marker = mMap.addMarker(new MarkerOptions()
-                    .position(latlng)
-                    .title(address)
-                    .icon(BitmapDescriptorFactory
-                            .defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
-            centre = CameraUpdateFactory.newLatLng(new LatLng(latlng.latitude, latlng.longitude));
-            zoom = CameraUpdateFactory.zoomTo(10);
-            mMap.moveCamera(centre);
-            mMap.animateCamera(zoom);
-            System.out.println(latlng);
+                  LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                  try {
+                     myAddress = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+                  } catch (IOException ioException) {
+                     ioException.printStackTrace();
+                  }
+                  String address = myAddress.get(0).getAddressLine(0);
+                  MarkerOptions markerOptions = new MarkerOptions().position(latLng).title(address);
+                  googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
+                  googleMap.addMarker(markerOptions);
 
+
+                  googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+                     @Override
+                     public void onMapClick(LatLng latlng) {
+                        // TODO Auto-generated method stub
+
+                        if (marker != null) {
+                           marker.remove();
+                        }
+
+                        try {
+                           myAddress = geocoder.getFromLocation(latlng.latitude, latlng.longitude, 1);
+                        } catch (IOException ioException) {
+                           ioException.printStackTrace();
+                        }
+                        String address = myAddress.get(0).getFeatureName();
+                        Log.d("address", "onMapClick: " + myAddress.get(0).getAdminArea() + "  " + myAddress.get(0).getSubAdminArea() +
+                                "     p " + myAddress.get(0).getPremises() + "  myAddress" + myAddress + "    SUBlOCALOITY" + myAddress.get(0).getSubLocality() + "  FACE" + myAddress.get(0).getThoroughfare());
+                        marker = googleMap.addMarker(new MarkerOptions()
+                                .position(latlng)
+                                .title(myAddress.get(0).getAddressLine(0))
+                                .icon(BitmapDescriptorFactory
+                                        .defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+                        centre = CameraUpdateFactory.newLatLng(new LatLng(latlng.latitude, latlng.longitude));
+                        zoom = CameraUpdateFactory.zoomTo(15);
+                        googleMap.moveCamera(centre);
+                        googleMap.animateCamera(zoom);
+                        System.out.println(latlng);
+
+                     }
+                  });
+
+                  spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+                     @Override
+                     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                        switch (position) {
+                           case 0:
+                              googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                              googleMap.setBuildingsEnabled(true);
+                              // Whatever you want to happen when the first item gets selected
+                              break;
+                           case 1:
+                              googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+
+                              // Whatever you want to happen when the second item gets selected
+                              break;
+                           case 2:
+                              googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+
+                              // Whatever you want to happen when the thrid item gets selected
+                              break;
+                           case 3:
+                              googleMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+                              break;
+
+                        }
+                     }
+
+                     @Override
+                     public void onNothingSelected(AdapterView<?> parent) {
+                        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                     }
+                  });
+                  zoom_In.setOnClickListener(new View.OnClickListener() {
+                     @Override
+                     public void onClick(View v) {
+                        googleMap.animateCamera(CameraUpdateFactory.zoomIn());
+                     }
+                  });
+                  zoom_out.setOnClickListener(new View.OnClickListener() {
+                     @Override
+                     public void onClick(View v) {
+                        googleMap.animateCamera(CameraUpdateFactory.zoomOut());
+                     }
+                  });
+                  searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                     @Override
+                     public boolean onQueryTextSubmit(String query) {
+                        String Location = searchView.getQuery().toString();
+                        List<Address> addressList = null;
+                        if (Location != null || !Location.equals("")) {
+                           try {
+                              addressList = geocoder.getFromLocationName(Location, 1);
+                           } catch (IOException ioException) {
+                              ioException.printStackTrace();
+                           }
+                           Address address1 = addressList.get(0);
+                           LatLng latLng1 = new LatLng(address1.getLatitude(), address1.getLongitude());
+
+                           googleMap.addMarker(new MarkerOptions().position(latLng1).title(Location));
+                           googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng1, 10));
+
+                        }
+                        return false;
+                     }
+
+                     @Override
+                     public boolean onQueryTextChange(String newText) {
+                        googleMap.clear();
+                        return false;
+                     }
+                  });
+
+               }
+            });
          }
       });
-//      mMap.setOnMyLocationButtonClickListener(new GoogleMap.OnMyLocationButtonClickListener() {
-//         @Override
-//         public boolean onMyLocationButtonClick() {
-//
-//            return false;
-//         }
-//      });
-      mMap.setOnMyLocationChangeListener(new GoogleMap.OnMyLocationChangeListener() {
-
-         @Override
-         public void onMyLocationChange(Location location) {
-
-       centre = CameraUpdateFactory.newLatLng(new LatLng(location.getLatitude(),location.getLongitude()));
-              zoom = CameraUpdateFactory.zoomTo(10);
-
-            markerOptions.position(new LatLng(location.getLatitude(),location.getLongitude()));
-
-            try {
-                 myAddress = geocoder.getFromLocation(location.getLatitude(),location.getLongitude(),1);
-
-            } catch (IOException ioException) {
-               ioException.printStackTrace();
-            }
-            String address = myAddress.get(0).getAddressLine(0);
-            markerOptions.title(address);
-            mMap.addMarker(markerOptions);
-            mMap.moveCamera(centre);
-            mMap.animateCamera(zoom);
-         }
-      });
-//      mMap.setOnMyLocationButtonClickListener(new GoogleMap.OnMyLocationButtonClickListener() {
-//         @Override
-//         public boolean onMyLocationButtonClick() {
-//
-//            return false;
-//         }
-//      });
    }
 
+
+   //
+//
+//
+   @Override
+   public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+      super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+      if (requestCode == 44) {
+         if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            getCurrentLocation();
+         }
+      }
+
+   }
 }
